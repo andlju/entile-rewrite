f7f8abcf7535dc5f66fc5e32362fcfa4136b267a
Anders Ljusberg <andlju@gmail.com>
2011-11-14 21:00:58 +0100

    Cleanup and new nuspecs

--- Build/build.ps1
+++
@@ -1,4 +0,0 @@
-$scriptPath = Split-Path $MyInvocation.InvocationName
-Remove-Module psake
-Import-Module (join-path $scriptPath '..\Source\packages\psake.4.0.1.0\tools\psake.psm1')
-invoke-psake -framework '4.0' (join-path $scriptPath 'default.ps1')
